dcf90325e52b83449a1874f59e94c829f3493737
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2026-04-25 12:20:53 +0600

    feat:dynamic routing,Api integration,authentication ui design completed

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -9,8 +9,8 @@ export default async function BlogDetails({ params }) {
   const paramsId = resolveParams.id;
   const matchedBlogsJson = await getAllBlogs(paramsId);
   return (
-    <div>
-      <div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
         <p>{matchedBlogsJson.id}</p>
         <p>{matchedBlogsJson.title}</p>
         <p>{matchedBlogsJson.body}</p>
